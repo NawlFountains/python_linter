10e6fff7abf59adeae542b5943b34be4ce11ed2c
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2023-03-03 17:52:21 -0300

    Syntax fix for bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
-#?\bin\bash
+#!\bin\bash
 
 #For all .py files run pylint and flake8
 echo "Linter start"
 
 for file in *.py do
 	echo file
-	#pylint file
-	#flake8 file
+	pylint file
+	flake8 file
 done
 
 echo "Linter finish"
