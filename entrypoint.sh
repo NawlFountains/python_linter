1788ef466fcf8632add46c1d6d6e6211d07e1fb4
Nahuel Fuentes <nahuelfuentes2001@gmail.com>
2023-03-03 19:49:21 -0300

    Change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
-#!\bin\bash
-
-#For all .py files run pylint and flake8
-echo "Linter start"
-
-for file in *.py do
-	echo file
-	pylint file
-	flake8 file
-done
-
-echo "Linter finish"
+#!/bin/bash
+echo "Start tree creation"
+tree
+echo "End tree creation"
